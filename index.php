a0e0762f6e29353f7dacdb783155ecde0b6abe9f
WemonsterA <hello@webmonster.tech>
2022-10-31 18:56:32 -0400

    Version 1.0.0

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $discord_invite =  $obj->instant_invite;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/pimentcss.min.css">
+    <link rel="stylesheet" href="/assets/css/pimentcss.min.css?<?=time();?>">
     <title>Webmonster Community</title>
     <script src="https://kit.fontawesome.com/45fd93cb0d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
@@ -358,12 +358,34 @@ $discord_invite =  $obj->instant_invite;
                 </a>
             </p>
 
-            <div class="row mb-3">
+            <div class="row mb-3 gap-2">
                 <div class="col-xs-12 col-md-6">
-                    <div class="card mb-2">
-                        <h4 class="text-red">
+                    <div class="block mb-2 bg-dark">
+                        <h4 class="text-orange">
+                            <svg width="24" height="24" fill="#fb8618" id="logo-techmonster" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+                                    <path id="face" class="emblem" d="M50,3.3C24.2,3.3,3.3,24.2,3.3,50S24.2,96.7,50,96.7S96.7,75.8,96.7,50S75.8,3.3,50,3.3z M56.1,26.3
+                                    c4.2,0,7.7,3.4,7.7,7.7s-3.4,7.7-7.7,7.7c-4.2,0-7.7-3.4-7.7-7.7S51.8,26.3,56.1,26.3z M40.2,29.5c2.8,0,5.1,2.3,5.1,5.1
+                                    c0,2.8-2.3,5.1-5.1,5.1c-2.8,0-5.1-2.3-5.1-5.1C35.2,31.8,37.4,29.5,40.2,29.5z M74.7,58.3c-0.2,0.8-0.5,1.5-0.9,2.3
+                                    c-2.3,5.2-5.2,8.4-8.5,10.3c-1.1,0.6-2.2,1.1-3.3,1.5c-3.6,1.2-7.6,1.4-12,1.4c-4.2,0-8.5-0.8-12.3-2.3c-1.2-0.5-2.4-1-3.5-1.6
+                                    c-4.1-2.1-7.5-5.1-9.1-8.5c-0.4-0.8-0.7-1.5-1-2.3c-2.4-6.6-1.8-13.2,0.4-15.8c0.9-1.1,2.7-1.2,5-0.8c1.1,0.2,2.4,0.6,3.7,1
+                                    c5.1,1.5,11.4,4,16.8,3.8c5.3-0.1,10.9-2.3,15.2-3.4c1.3-0.3,2.6-0.6,3.6-0.7c0.6,0,1.2,0,1.7,0.1C74.1,44.4,77,50.8,74.7,58.3z"></path>
+                                <circle id="oeil-gauche" class="emblem" cx="42.2" cy="35" r="2"></circle>
+                                <circle id="oeil-droit" class="emblem" cx="52.7" cy="35" r="2.9"></circle>
+                            </svg>
+                            Le Club
+                        </h4>
+                        <p class="text-orange">Retrouver les artisans du web et de la tech qui composent notre communauté.</p>
+                        <a href="https://club.webmonster.tech/" class="option-orange">
+                            <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
+                            <span class="button-text">Le site</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <div class="block mb-2 bg-red">
+                        <h4 class="text-white">
                             <svg width="24" height="24" viewBox="0 0 100 55">
-                                <path fill="#E9473E" stroke="#E9473E" stroke-width="4.7131" stroke-linejoin="round" d="M6.34,27.7l1.09,0.32
+                                <path fill="#ffffff" stroke="#ffffff" stroke-width="4.7131" stroke-linejoin="round" d="M6.34,27.7l1.09,0.32
                                 l1.64,1.09l2.04,1.21l2.02,1l1.84,0.78l2.21,0.77l3.15,1l2.74,0.5l2.58,0.48l1.6,0.21l1.79,0.06l2.61,0.15l2.23-0.22l2.57-0.27
                                 l2.37-0.33l2.88-0.82l2.93-0.94l2.75-1.17l2.5-1.2l2.57-1.48l3.21-2l2.73-1.76l2.47-1.7l2.95-1.94l3.26-2.12l2.25-1.22l2.2-1.1
                                 l1.85-0.85l1.84-0.59l1.73-0.41l1.77-0.16l1.72-0.01l2.03,0.28l1.71,0.51l1.27,0.59l1.04,0.77l1,0.78l0.79,0.8l0.97,1.2l0.59,0.91
@@ -383,7 +405,7 @@ $discord_invite =  $obj->instant_invite;
                             </svg>
                             Piment CSS
                         </h4>
-                        <p>Piment CSS est un micro-framework css réalisé par la communauté Webmonster.</p>
+                        <p class="text-white">Piment CSS est un micro-framework css réalisé par la communauté Webmonster.</p>
                         <a href="https://github.com/freepiment/Piment-Css" class="option-dark">
                             <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
                             <span class="button-text">Github</span>
@@ -391,9 +413,9 @@ $discord_invite =  $obj->instant_invite;
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-6">
-                    <div class="card mb-2">
-                        <h4 class="text-blue">
-                            <svg width="24" height="24" fill="#0f0ff5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+                    <div class="block mb-2 bg-blue">
+                        <h4 class="text-white">
+                            <svg width="24" height="24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
                                 <path id="face" class="emblem" d="M50,3.3C24.2,3.3,3.3,24.2,3.3,50S24.2,96.7,50,96.7S96.7,75.8,96.7,50S75.8,3.3,50,3.3z M56.1,26.3
                                 c4.2,0,7.7,3.4,7.7,7.7s-3.4,7.7-7.7,7.7c-4.2,0-7.7-3.4-7.7-7.7S51.8,26.3,56.1,26.3z M40.2,29.5c2.8,0,5.1,2.3,5.1,5.1
                                 c0,2.8-2.3,5.1-5.1,5.1c-2.8,0-5.1-2.3-5.1-5.1C35.2,31.8,37.4,29.5,40.2,29.5z M74.7,58.3c-0.2,0.8-0.5,1.5-0.9,2.3
@@ -405,18 +427,18 @@ $discord_invite =  $obj->instant_invite;
                             </svg>
                             Webmonster community
                         </h4>
-                        <p>Github de la communauté Webmonster.</p>
-                        <a href="https://github.com/Webmonster-Community" class="option-dark">
+                        <p class="text-white">Github de la communauté Webmonster.</p>
+                        <a href="https://github.com/Webmonster-Community" class="option-orange">
                             <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
                             <span class="button-text">Github</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-6">
-                    <div class="card mb-2">
-                        <h4 class="text-blue">
+                    <div class="block mb-2 bg-blue-light-1">
+                        <h4 class="text-green">
                             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 97.7 108.6" style="enable-background:new 0 0 97.7 108.6;" xml:space="preserve">
-                                <path fill="#1211f5" d="M89.5,5.9c0,2.4,0,4.6,0,6.8c0,0.3-0.5,0.8-0.9,0.9c-0.5,0.2-1,0.1-1.6,0.1c-21.1,0-42.1,0-63.2,0
+                                <path fill="#1ef671" d="M89.5,5.9c0,2.4,0,4.6,0,6.8c0,0.3-0.5,0.8-0.9,0.9c-0.5,0.2-1,0.1-1.6,0.1c-21.1,0-42.1,0-63.2,0
                                 c-0.9,0-1.9,0-2.8,0.1c-2.2,0.3-3.7,2-3.6,4.2c0.1,2.2,1.6,3.9,3.8,4.1c0.6,0.1,1.1,0,1.7,0c21.5,0,43,0,64.5,0c0.7,0,1.3,0,2.1,0
                                 c0,27.4,0,54.6,0,82c-0.6,0-1.1,0-1.7,0c-22.5,0-45.1,0-67.6,0c-7.3,0-12.6-5.4-12.7-12.7c0-24.4,0-48.7,0-73.1
                                 c0-7.3,5.1-12.6,12.3-12.6c22.8-0.1,45.6,0,68.4,0C88.6,5.8,88.9,5.8,89.5,5.9z M70.3,53.5c0-2.3,0-4.6,0-6.9c0-2.9,0-2.9-2.7-3.9
@@ -428,15 +450,15 @@ $discord_invite =  $obj->instant_invite;
                             </svg>
                             Guides du·de la développeur·euse
                         </h4>
-                        <p>Pour débuter son apprentissage du métier de développeur·euse en Martinique il est important de comprendre en quoi il consiste, comment il évolue et ses différents aspects...</p>
-                        <a href="https://guides.techmonster.info/" class="option-dark">
+                        <p class="text-green">Pour débuter son apprentissage du métier de développeur·euse en Martinique il est important de comprendre en quoi il consiste, comment il évolue et ses différents aspects...</p>
+                        <a href="https://guides.techmonster.info/" class="option-green">
                             <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
                             <span class="button-text">Voir le guide</span>
                         </a>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-6">
-                    <div class="card mb-2">
+                    <div class="block mb-2">
                         <h4 class="text-dark">
                             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M21.628 15.306l2.372 1.379-12.668 7.315-11.332-6.584 2.372-1.368 8.957 5.206 10.299-5.948zm-10.487-11.438l.017-.003.175.182-.017.003c-.119.023-.216.06-.302.108-.102.06-.125.119-.063.155.062.037.153.023.369-.036.302-.075.518-.069.688.03.181.105.254.3-.111.51-.148.086-.325.142-.422.158l-.017.003-.186-.181.022-.007c.131-.023.273-.072.371-.128.108-.062.137-.125.063-.168-.068-.039-.159-.033-.352.02-.302.089-.541.083-.705-.013-.22-.128-.186-.326.094-.487.119-.07.245-.116.376-.146zm.015 1.357l-.314.181-.964-.382.639.569-.319.185-1.514-.544.318-.184 1.004.405-.678-.592.308-.178.987.395-.679-.573.307-.178.905.896zm-2.326.213l-.468.27.221.128.445-.256.25.145-.445.256.255.149.496-.286.25.145-.799.461-1.224-.712.77-.444.249.144zm-.001 1.13l-.308.178-1.271-.231.833.484-.28.161-1.223-.711.342-.198 1.158.205-.754-.438.279-.161 1.224.711zm6.338-2.183l-7.6 4.389-.629-.366 7.601-4.389.628.366zm1.263.735l-1.898 1.096-.629-.365 1.899-1.097.628.366zm-1.273 2.192l-2.538 1.465-1.887-1.096 2.538-1.465 1.887 1.096zm-4.428 1.1l-1.899 1.096-.629-.366 1.899-1.096.629.366zm6.959-2.561l-1.898 1.096-.629-.366 1.899-1.096.628.366zm-5.702 3.291l-1.899 1.097-.629-.366 1.899-1.096.629.365zm8.225-3.291l-7.551-4.389-10.138 5.853 7.552 4.389 10.137-5.853zm3.789.733l-12.671 7.315-11.329-6.584 12.668-7.315 11.332 6.584zm-11.671 9.047l-1.003.58-1.001-.583-7.968-4.631-2.357 1.36 11.332 6.584 12.668-7.315-2.359-1.372-9.312 5.377z"></path>
@@ -451,21 +473,27 @@ $discord_invite =  $obj->instant_invite;
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-6">
-                    <div class="card mb-2">
+                    <div class="block mb-2">
                         <h4 class="text-dark"><i class="fa-brands fa-github"></i> Liste en agences Web & Marketing</h4>
                         <p>Liste des agences web et marketing en Martinique, Guadeloupe et Guyane disponible dans différents formats.</p>
-                        <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-martinique" class="option-dark">
-                            <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
+                        <div class="block">
+                            <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-martinique" class="option-dark">
+                                <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
                             <span class="button-text">Martinique</span>
-                        </a>
-                        <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-guadeloupe" class="option-dark">
-                            <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
-                            <span class="button-text">Guadeloupe</span>
-                        </a>
-                        <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-guyane" class="option-dark">
-                            <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
-                            <span class="button-text">Guyane</span>
-                        </a>
+                            </a>
+                        </div>
+                        <div class="block">
+                            <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-guadeloupe" class="option-dark">
+                                <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
+                                <span class="button-text">Guadeloupe</span>
+                            </a>
+                        </div>
+                        <div class="block">
+                            <a href="https://github.com/Webmonster-Community/liste-agence-web-marketing-guyane" class="option-dark">
+                                <span class="circle" aria-hidden="true"><span class="icon arrow"></span></span>
+                                <span class="button-text">Guyane</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
